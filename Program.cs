89c77d0f5be6e37a58aa62b389113791d259ed0e
MichalHrbek <michal@dee.cz>
2023-02-19 14:31:28 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ using (StreamWriter writer = new StreamWriter(outpath))
 {
 	while (!sp.done)
 	{
-        Console.WriteLine($"Succes: {sp.successful}; Error: {sp.successful}; Total: {sp.successful + sp.successful} / {ips.Length / 6}");
+        Console.WriteLine($"Succes: {sp.successful}; Error: {sp.unsuccessful}; Total: {sp.successful + sp.unsuccessful} / {ips.Length / 6}");
         while (sp.output.Count !=0)
         {
             writer.WriteLine(sp.output.Dequeue());
